2e49f65adb43ccbf9a4980feb9bd321a63926c40
allyson_souza <souzaallyson53@gmail.com>
2025-09-24 21:55:51 -0300

    Inicio do Cadastro

--- Main.py
+++ Main.py
@@ -7,11 +7,11 @@ from Banco import Banco
 class MyApp(MDApp):
     def build(self):
         return Builder.load_file("Views/Front.kv")
-
+    #quando iniciar
     def on_start(self):
         if self.root:
             print("ScreenManager inicializado com sucesso!")
-
+    #tela escolha
     def irLoginAluno(self):
         if self.root:
             self.root.current = "LoginAluno"
@@ -24,6 +24,7 @@ class MyApp(MDApp):
         if self.root:
             self.root.current = "LoginProfissional"
 
+    #tela LoginProfissional
     def irCadastroProfissional1(self):
         if self.root:
             self.root.current = "CadastroProfissional1"
@@ -36,9 +37,10 @@ class MyApp(MDApp):
         else:
             print("root ainda não existe")
 
+    #tela CadastroProfissional1
     def ListaProfissõesText_Click(self, item, ativa):
         if ativa:
-            menu_items = self.AddItens(Banco.consultar('NOME','PROFISSOES','1'))
+            menu_items = self.AddItensProfissoes(Banco.consultar('NOME','PROFISSOES','1'))
             MDDropdownMenu(caller=item, items=menu_items).open()
         else:
             print('erro')
@@ -46,7 +48,7 @@ class MyApp(MDApp):
 
     def ListaProfissõesBotao_Click(self, item):
         try:
-            menu_items = self.AddItens(Banco.consultar('NOME','PROFISSOES','1'))
+            menu_items = self.AddItensProfissoes(Banco.consultar('NOME','PROFISSOES','1'))
             MDDropdownMenu(caller=item, items=menu_items).open()
         except Exception as e:
             print(e)
@@ -54,7 +56,7 @@ class MyApp(MDApp):
     def ListaProfissoesItens_Click(self, text_item):
         self.root.get_screen("CadastroProfissional1").ids.List_ProfissoesText.text = text_item
 
-    def AddItens(self, itens):
+    def AddItensProfissoes(self, itens):
         menu_items = [
             {
                 "text": f"{item[0].translate(str.maketrans("", "", "(),'"))}",
@@ -63,5 +65,96 @@ class MyApp(MDApp):
         ]
         return menu_items
 
+    def irCadastroProfissional2(self):
+        if self.root:
+            self.root.current = "CadastroProfissional2"
+
+    #tela CadastroProfissionais2
+    def ListaUFText_Click(self,item, ativa):
+        if ativa:
+            itens = [
+                'Acre',
+                'Alagoas',
+                'Amapá',
+                'Amazonas',
+                'Bahia',
+                'Ceará',
+                'Distrito Federal',
+                'Espirito Santo',
+                'Goiás',
+                'Maranhão',
+                'Mato Grosso do Sul',
+                'Mato Grosso',
+                'Minas Gerais',
+                'Pará',
+                'Paraíba',
+                'Paraná',
+                'Pernambuco',
+                'Piauí',
+                'Rio de Janeiro',
+                'Rio Grande do Norte',
+                'Rio Grande do Sul',
+                'Rondônia',
+                'Roraima',
+                'Santa Catarina',
+                'São Paulo',
+                'Sergipe',
+                'Tocantins'
+            ]
+            menu_items = [
+                {
+                "text":f'{index}',
+                "on_release": lambda x=f'{index}': self.ListUFItens_Click(x)
+                }for index in itens
+            ]
+
+            MDDropdownMenu(caller=item, items=menu_items).open()
+        else:
+            print('erro')
+
+    def ListaUFBotao_Click(self, item):
+        try:
+            itens = [
+                'Acre',
+                'Alagoas',
+                'Amapá',
+                'Amazonas',
+                'Bahia',
+                'Ceará',
+                'Distrito Federal',
+                'Espirito Santo',
+                'Goiás',
+                'Maranhão',
+                'Mato Grosso do Sul',
+                'Mato Grosso',
+                'Minas Gerais',
+                'Pará',
+                'Paraíba',
+                'Paraná',
+                'Pernambuco',
+                'Piauí',
+                'Rio de Janeiro',
+                'Rio Grande do Norte',
+                'Rio Grande do Sul',
+                'Rondônia',
+                'Roraima',
+                'Santa Catarina',
+                'São Paulo',
+                'Sergipe',
+                'Tocantins'
+            ]
+            menu_items = [
+                {
+                    "text": f'{item[index]}',
+                    "on_release": lambda x=f'{item[index]}': self.ListaUFText_Click(x)
+                } for index in itens
+            ]
+
+            MDDropdownMenu(caller=item, items=menu_items).open()
+        except Exception as e:
+            print(e)
+
+    def ListUFItens_Click(self, text_item):
+        self.root.get_screen("CadastroProfissional2").ids.List_UFText.text = text_item
 
 MyApp().run()
